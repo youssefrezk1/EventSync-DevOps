0b00fb68486987ccd60372903d9fef4aafa2ab5d
youssefrezk1 <youssefrezk012@gmail.com>
2026-08-20 10:52:19 +0300

    Manage VPC CNI network policy with Terraform

---
+++ terraform/eks-addons.tf
@@ -0,0 +1,29 @@
+# ---------------------------------------------------------
+# EKS Managed Add-ons
+# ---------------------------------------------------------
+
+resource "aws_eks_addon" "vpc_cni" {
+  cluster_name  = aws_eks_cluster.main.name
+  addon_name    = "vpc-cni"
+  addon_version = "v1.22.4-eksbuild.3"
+
+  # Adopt the existing self-managed VPC CNI installation.
+  resolve_conflicts_on_create = "OVERWRITE"
+
+  # Preserve explicitly managed/custom settings on future updates.
+  resolve_conflicts_on_update = "PRESERVE"
+
+  configuration_values = jsonencode({
+    enableNetworkPolicy = "true"
+
+    env = {
+      NETWORK_POLICY_ENFORCING_MODE = "standard"
+    }
+  })
+
+  depends_on = [
+    aws_eks_cluster.main
+  ]
+
+  tags = local.common_tags
+}
